665fcfd58c91d97a94d86306b1d2fe5790604be6
Gmukoro <gmukoro3@gmail.com>
2023-03-22 08:41:04 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@
  *
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
-	int i = 0;
+	int count = 0;
 	char letter;
 
-	while (i++ <= 9)
+	while (count++ <= 9)
 	{
 
 		for (letter = 'a' ; letter <= 'z' ; letter++)
