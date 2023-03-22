0781e7ef05c74fb2d66d580b005fb8e585d4ce11
Gmukoro <gmukoro3@gmail.com>
2023-03-21 19:20:23 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.c"
+/**
+ * main - Entry Point
+ * Description: A program that prints the alphabet in lower
+ * case follow by a new line
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 'a' ; i < 'z' ; i++)
+		_putchar (i);
+	_putchar ("\n");
+
+
+}
