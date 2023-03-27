85ed3f29c33b57f3762dcdfe69f894881113f610
Gmukoro <gmukoro3@gmail.com>
2023-03-27 09:56:07 -0700

    My first commit on pointers, arrays and strings. How to assign/values int points to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* reset_to_98 - A function that takes a pointer to an int and upd
+* ates its points to 98
+*
+* @n: The int under consideratn
+*
+* Return: Always 0
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
