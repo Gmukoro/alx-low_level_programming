926ba86fe08f9d0873300a4ed3aef768fcec8f8d
Gmukoro <gmukoro3@gmail.com>
2023-04-03 21:21:43 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - A function prints the sums of the two diagon-
+als of a square matrix
+ * @a: pointer to start of the matrix
+ * @size: width of the matrix column
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int z, y, p, t = 0, u = 0;
+
+	for (z = 0; z < size; z++)
+	{
+		p = (z * size) + z;
+		t += *(a + p);
+	}
+	for (y = 0; y < size; y++)
+	{
+		p = (y * size) + (size - 1 - y);
+		u += *(a + p);
+	}
+	printf("%i, %i\n", t, u);
+}
