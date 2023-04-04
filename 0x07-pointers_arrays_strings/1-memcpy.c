3187e30f7e9a10a74ce8489232ebe3f9ca15ee39
Gmukoro <gmukoro3@gmail.com>
2023-04-03 19:37:10 -0700

    A program that copies mem area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* *_memcpy - A program that copies memory area
+* @dest: Destinatin string
+* @src: Source string
+* @n: Bytes to be copied
+*
+* Return: Destination string
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int t = 0;
+
+	while (t < n)
+	{
+		src[t] = dest[t];
+		t++;
+	}
+	return (dest);
+}
