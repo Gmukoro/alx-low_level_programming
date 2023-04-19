e9c69205a3e43969d7a9c2f261c1a52cbd96dae9
Gmukoro <gmukoro3@gmail.com>
2023-04-18 19:30:33 -0700

    Header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Funtion that define a new type struct dog
+* @name: Dog name entry
+* @age: Dog age entry
+* @owner: Dog owner details
+*/
+
+struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+#endif
