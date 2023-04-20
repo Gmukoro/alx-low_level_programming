c326419b0b75c4266b8bead86a8d00620f4ff27f
Gmukoro <gmukoro3@gmail.com>
2023-04-20 04:11:57 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,8 +15,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t t;
 
-	if (array == 0 || size == 0)
+	if (array == 0 && action == 0)
 		return;
+	
 	for (t = 0; t < size; t++)
 
 	{
