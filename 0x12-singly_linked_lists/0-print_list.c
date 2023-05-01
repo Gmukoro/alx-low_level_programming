9f938b595f98090246c4ab16a5ab6dbfc72ad3ad
Gmukoro <gmukoro3@gmail.com>
2023-05-01 13:00:05 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,14 +16,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[%d]%s\n", 0, "(nil)");
-		}
 		else
-		{
 			printf("[%d]%s\n", h->len, h->str);
-		}
 		elements++;
+
 		h = h->next;
 	}
 	return (elements);
