a23e95e4f5311f42a47c99fb0088a508e5e72e55
Gmukoro <gmukoro3@gmail.com>
2023-05-01 10:36:53 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+/**
+* singly_print - A function that prints
+*
+*/
+void singly_print((constructor))bmain()
+{
+	printf("You're beat! and yet, you must allow,\n
+I bore my house upon my back!\n");
+}
