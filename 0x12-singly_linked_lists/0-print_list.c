c6a89c4dd7ec0145261ec01fc0e5b3a6f4cd9c68
Gmukoro <gmukoro3@gmail.com>
2023-05-01 12:45:54 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+* print_list - A program that prints all elements of a list_t
+* @h: List of elements in singly list
+*
+* Return: Elements
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t elements;
+
+	elements = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d]%s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d]%s\n", h->len, h->str);
+		}
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
