d9cca5b7da84993f8cd70641122da97b2677978b
Gmukoro <gmukoro3@gmail.com>
2023-05-08 12:33:28 -0700

    Updated 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,18 +10,9 @@
 */
 void print_binary(unsigned long int n)
 {
-	unsigned long int test = 1UL << (sizeof(unsigned long int)*8 - 1);
+	if (n > 1)
 
-	while (test)
-	{
-        	if (n & test)
-		{
-			_putchar('1');
-		}
-		else
-		{
-			_putchar('0');
-		}
-	test >>= 1;
-	}
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
 }
