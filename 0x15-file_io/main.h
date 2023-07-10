25b383ed4376846100369ec60e7072f8a333fd7d
Gmukoro <gmukoro3@gmail.com>
2023-07-10 13:28:07 -0700

    Header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int _putchar (char c);
 ssize_t read_textfile(const char *filename, size_t letters);
