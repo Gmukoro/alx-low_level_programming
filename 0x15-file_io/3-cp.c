e90000de90159ed130a3cd5dea68b150c140a0e7
Gmukoro <gmukoro3@gmail.com>
2023-07-11 00:47:27 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,70 +1,51 @@
 #include "main.h"
 
 /**
- * cp - A program copies src file to desinations
- * @file_to: The destination file
- * @file_from: The source file
- * Return: ...
- */
-void close_file(int filedesc)
-{
-	int print;
-
-	print = close(filedesc);
-
-	if (print < 0)
-	{
-		if (print < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filedesc);
-		exit(100);
-	}
-}
-
-/**
  * main - Entry point
  * @ac: Argument count
  * @av: Argument vector
  * Return: always 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int rd, cop_a, cop_b, wt;
-	char *buf;
+	int fd_t, fd_w, t, s, n;
+	char buf[BUFSIZ];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	cop_a = open(argv[1], O_RDONLY);
-	if (cop_a < 0)
+	fd_t = open(argv[1], O_RDONLY);
+	if (fd_t < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	buf = malloc(sizeof(char) * 1024);
-	if (!buf)
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((x = read(fd_t, buf, BUFSIZ)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s/n", argv[2]);
-		exit(99);
+		if (fd_w < 0 || write(fd_w, buf, t) != t)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_t);
+			exit(99);
+		}
 	}
-	rd = read(cop_a, buf, 1024);
-	if (rd < 0)
+	if (t < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	cop_b = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	wt = write(cop_b, buf, rd);
-	if (wt < 0 || rd != wt)
+	s = close(fd_t);
+	n = close(fd_w);
+	if (s < 0 || n < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s/n", argv[2]);
-		close(cop_a);
-		free(buf);
-		exit(99);
+		if (s < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_t);
+		if (n < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
+		exit(100);
 	}
-	close_file(cop_a);
-	close_file(cop_b);
 	return (0);
-
 }
